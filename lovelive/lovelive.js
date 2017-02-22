45fe6264c2db9e49e1f6190689bce2fcd95851c0
liushengxian <liushengxian1230@163.com>
2017-02-23 01:31:38 +0800

    add lyric support for lovelive

--- lovelive/lovelive.js
+++ lovelive/lovelive.js
@@ -1,22 +1,47 @@
 var song_playing = false;
 var play_interval = -1;
 var angel = 0;
+var time = 0;
+var timeInterval = 40;
 
 var music = document.getElementById('music-treasure');
+
+// lyc_parser.testFunc();
+lyc_parser.load('lovelive.lrc');
 
 $('.circle-img').on('hover',function(){
 	$(this).css('box-shadow','2px solid #ff0033');
 });
+
+function resetStatus(){
+	$('#lyric').text('==============Present By Misanya=============');
+}
+
+
+function playMusic(){
+	var word = lyc_parser.check(music.currentTime);
+	if(word){
+		$('#lyric').text(word);	
+	}
+	rotateCircle();
+}
 
 function rotateCircle(){
 	angel+= 0.2;
 	$('.circle-img').css('transform','rotate('+angel+'deg)');
 }
 
+music.onended = function(){
+	resetStatus();
+	lyc_parser.reset();
+};
+
+resetStatus();
+
 $('.circle-img').on('click',function(){
 	song_playing = !song_playing;
 	if(song_playing){
-		play_interval = window.setInterval(rotateCircle,40);
+		play_interval = window.setInterval(playMusic,40);
 		music.play();
 		//play the music.
 	}else{
--- lovelive/lyric-parser.js
+++ lovelive/lyric-parser.js
@@ -1,1 +1,70 @@
-console.info('go parser!');
+// console.info('go parser!');
+	
+var lyc_parser = (function(){
+
+	function _replacer(match,d1,d2,d3,d4,offset,string){
+		return [parseInt(d1)*60+parseInt(d2),d3].join('.');
+		// return parseInt(d1)*60+parseInt(d2)+'{['+d4+']}';
+	}
+
+	//TODO _handler not used here.
+	var _handler = null;
+	var _lyc_grp = [];
+	var _current_index = 0;
+
+	var _parse = function(text){
+		var lyc_list = text.split('\n');
+		// console.info('length:'+lyc_list.length);
+
+		var reg = /\[(\d{2}):(\d{2}).(\d{2})\]([\s\S]*)/;
+		for (var i = 0; i < lyc_list.length; i++) {
+			_lyc_grp.push({time:parseFloat(lyc_list[i].replace(reg,_replacer)),word:lyc_list[i].replace(reg,'$4')})
+		}
+
+		// console.dir(_lyc_grp);
+	}
+
+	var _testFunc = function(){
+		console.info('test function ok!');
+	}
+
+	var _load = function(lyc_file,handler){
+
+		_handler = handler;
+
+		var client = new XMLHttpRequest();
+		client.open('GET', lyc_file);
+		client.onreadystatechange = function() {
+		if(client.readyState == client.DONE){
+			_parse(client.responseText);
+		}}
+		client.send();
+	}
+
+	var _judge = function(time){
+
+		if(_current_index >= _lyc_grp.length){
+			return null;
+		}
+
+		if(_lyc_grp[_current_index].time < time){
+			return _lyc_grp[_current_index++].word;
+		}
+		else{
+			return null;	
+		}	
+	}
+
+	var _reset = function(){
+		_current_index = 0;
+	}
+
+
+	return {
+		testFunc: _testFunc,
+		load: _load,
+		check: _judge,
+		reset: _reset
+
+	}
+})();
