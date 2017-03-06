99007d67f6c786ad81dfc576ac7aa9366f827361
liushengxian <liushengxian1230@163.com>
2017-03-07 01:15:37 +0800

    add lock try

--- orientation/index.js
+++ orientation/index.js
@@ -2,4 +2,5 @@ console.info('hello test orientation!');
 
 if(window.screen.orientation){
 	alert('screen orientation:'+ screen.orientation.type);
+	window.screen.orientation.lock('portrait-primary');
 }
