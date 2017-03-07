75eb1d2d44f8ec32cfb1029a55a02cba4f75ae41
Misanya Liu <liushengxian1230@163.com>
2017-03-07 10:03:45 +0800

    Update index.js
    add a console output

--- orientation/index.js
+++ orientation/index.js
@@ -3,7 +3,8 @@ console.info('hello test orientation!');
 if(window.screen.orientation){
 	alert('screen orientation:'+ screen.orientation.type);
 	try{
-		window.screen.orientation.lock('portrait-primary');	
+		window.screen.orientation.lock('portrait-primary');
+		console.log('hello lock');
 	}
 	catch(err){
 		alert(err.message);
